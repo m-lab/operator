7f8418f7b51138dd7f25dbc47ee2d74f774ce5d6
Nathan Kinkade <nath@nkinka.de>
2015-04-09 10:41:21 -0400

    Removed outdated user entry for Josh, since that PlanetLab account isn't active any longer.

--- plsync/users.py
+++ plsync/users.py
@@ -2,7 +2,6 @@
 # NOTE: User roles are not managed here.  Visit PlanetLab to change user roles.
 user_list = [('Stephen', 'Stuart', 'sstuart@google.com'),
              ('Will',    'Hawkins', 'hawkinsw@opentechinstitute.org'),
-             ('Josh',    'King', 'joshking@newamerica.net'),
              ('Jordan',  'McCarthy', 'mccarthy@opentechinstitute.org'),
              ('Chris',   'Ritzo', 'critzo@opentechinstitute.org'),
              ('Josh',    'Bailey', 'joshb@google.com'),
