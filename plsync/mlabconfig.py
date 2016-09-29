78a02f0f5964097df760cdc8adb49f30102151f3
Stephen Soltesz <stephen.soltesz@gmail.com>
2016-09-29 14:24:12 -0400

    Add zone generation support to mlabconfig.py (#39)
    * Add "zone" as a supported format for mlabconfig.
    
    This change adds a new output format for mlabconfig to generate a zone
    file for measurement-lab.org. This form of generation should obsolete
    gen-zone.py.
    
    The mlabzone.header.in is taken from the current gen-zone.py script. The
    included version of mlabzone.header.in increases the zone expiration
    time from 1 day to 7 days. This should allow for longer periods of
    outages of the shadow master server.
    
    * Use style-specified quotaion marks on string constants.
    * Remove legacy comments from header template.
    * 60 min refresh cycle / 10 min retry
    * Update primary / secondary DNS servers.
    * Add TODO to see if we can retire DONAR.
    * Allow serial number to be provided on the command line.
    * Store prefix and revision in a single tmp json file to increment revision numbers.
    * Correct 4-space indenation throughout.
    * Update sitename TODO to a statement.

--- plsync/mlabconfig.py
+++ plsync/mlabconfig.py
@@ -1,12 +1,21 @@
 #!/usr/bin/env python
 
 import json
+import logging
 import optparse
 import os
 import re
 import StringIO
 import sys
 import time
+
+ZONE_TTL = 60 * 5
+ZONE_MIN_TTL = 60 * 5
+ZONE_REFRESH = 60 * 60
+ZONE_RETRY = 60 * 10
+ZONE_EXPIRE = 7 * 60 * 60 * 24
+ZONE_HEADER_TEMPLATE = 'mlabzone.header.in'
+ZONE_SERIAL_COUNTER = '/tmp/mlabconfig.serial'
 
 
 def usage():
@@ -22,7 +31,6 @@ EXAMPLES:
     mlabconfig.py --format=sitestats
       (e.g. mlab-site-stats.json)
 
-TODO:
     mlabconfig.py --format=zone
       (e.g. gen_zones.py)
 """
@@ -30,6 +38,19 @@ TODO:
 
 def parse_flags():
     parser = optparse.OptionParser(usage=usage())
+    parser.set_defaults(force=False,
+                        support_email='support.measurementlab.net',
+                        primary_nameserver='sns-pb.isc.org',
+                        secondary_nameserver='ns-mlab.greenhost.net',
+                        domain='measurement-lab.org',
+                        serial='auto',
+                        zonefile=None,
+                        ttl=ZONE_TTL,
+                        minttl=ZONE_MIN_TTL,
+                        refresh=ZONE_REFRESH,
+                        retry=ZONE_RETRY,
+                        expire=ZONE_EXPIRE)
+
     parser.add_option(
         '', '--sites_config', metavar='sites', dest='sites_config',
         default='sites', help='The name of the module with Site() definitions.')
@@ -45,11 +66,196 @@ def parse_flags():
         default='slice_list',
         help=('The variable name of experiments within the experiments_config '
               'data.'))
-    # TODO: Support multiple formats, e.g. 'hostips', 'zone', 'sitestats'.
     parser.add_option(
         '', '--format', metavar='format', dest='format', default='hostips',
         help='Format of output.')
-    return parser.parse_args()
+    parser.add_option(
+        '', '--zoneheader', metavar='zoneheader.in', dest='zoneheader',
+        default=ZONE_HEADER_TEMPLATE,
+        help='The full path to zone header file.')
+
+    (options, args) = parse_flags()
+
+    # Check given parameters.
+    if options.format == 'zone' and not os.path.exists(options.zoneheader):
+        logging.error('Zone header file %s not found!', options.zoneheader)
+        sys.exit(1)
+
+    return (options, args)
+
+
+def comment(output, note):
+    output.write('\n; %s\n' % note)
+
+
+def write_a_record(output, hostname, ipv4):
+    output.write(format_a_record(hostname, ipv4))
+    output.write('\n')
+
+
+def format_a_record(hostname, ipv4):
+    return '%-32s  IN  A   \t%s' % (hostname, ipv4)
+
+
+def write_aaaa_record(output, hostname, ipv6):
+    output.write(format_aaaa_record(hostname, ipv6))
+    output.write('\n')
+
+
+def format_aaaa_record(hostname, ipv6):
+    return '%-32s  IN  AAAA\t%s' % (hostname, ipv6)
+
+
+def export_router_and_switch_records(output, sites):
+    comment(output, 'router and switch v4 records.')
+    for i, site in enumerate(sites):
+        write_a_record(output, 'r1.' + site['name'], site.ipv4(index=1))
+        write_a_record(output, 's1.' + site['name'], site.ipv4(index=2))
+
+
+def export_pcu_records(output, sites):
+    comment(output, 'pcus v4')
+    for site in sites:
+        # TODO: change site['nodes'] to a pre-sorted list type.
+        for node in sorted(site['nodes'].values(), key=lambda n: n.hostname()):
+            write_a_record(output, node['pcu'].recordname(), node['pcu'].ipv4())
+
+
+def export_server_records(output, sites):
+    export_server_records_v4(output, sites)
+    export_server_records_v4(output, sites, decoration='v4')
+    export_server_records_v6(output, sites)
+    export_server_records_v6(output, sites, decoration='v6')
+
+
+def export_server_records_v4(output, sites, decoration=''):
+    comment(output, 'hosts v4%s' % (' decorated' if decoration else ''))
+    for site in sites:
+        # TODO: change site['nodes'] to a pre-sorted list type.
+        for node in sorted(site['nodes'].values(), key=lambda n: n.hostname()):
+            write_a_record(output, node.recordname(decoration), node.ipv4())
+
+
+def export_server_records_v6(output, sites, decoration=''):
+    comment(output, 'hosts v6%s' % (' decorated' if decoration else ''))
+    for site in sites:
+        # TODO: change site['nodes'] to a pre-sorted list type.
+        for node in sorted(site['nodes'].values(), key=lambda n: n.hostname()):
+            if node.ipv6_is_enabled():
+                write_aaaa_record(
+                    output, node.recordname(decoration), node.ipv6())
+
+
+def export_experiment_records(output, sites, experiments):
+    for experiment in experiments:
+        if experiment['index'] is None:
+            # Ignore experiments without an IP address.
+            continue
+        export_experiment_records_v4(output, sites, experiment)
+        export_experiment_records_v4(output, sites, experiment, decoration='v4')
+
+        export_experiment_records_v6(output, sites, experiment)
+        export_experiment_records_v6(output, sites, experiment, decoration='v6')
+
+
+def export_experiment_records_v4(output, sites, experiment, decoration=''):
+    comment(output, '%s v4%s' % (experiment.dnsname(), (
+        ' decorated' if decoration else '')))
+    for site in sites:
+        # TODO: change site['nodes'] to a pre-sorted list type.
+        for node in sorted(site['nodes'].values(), key=lambda n: n.hostname()):
+            # TODO: remove sitenames (or exclude mlab4's).
+            write_a_record(output, experiment.sitename(node, decoration),
+                           experiment.ipv4(node))
+            write_a_record(output, experiment.recordname(node, decoration),
+                           experiment.ipv4(node))
+
+
+def export_experiment_records_v6(output, sites, experiment, decoration=''):
+    comment(output, '%s v6%s' % (experiment.dnsname(), (
+        ' decorated' if decoration else '')))
+    for site in sites:
+        # TODO: change site['nodes'] to a pre-sorted list type.
+        for node in sorted(site['nodes'].values(), key=lambda n: n.hostname()):
+            # TODO: remove sitenames (or exclude mlab4's).
+            if (node.ipv6_is_enabled() and experiment.ipv6(node)):
+                write_aaaa_record(output, experiment.sitename(node, decoration),
+                                  experiment.ipv6(node))
+                write_aaaa_record(output, experiment.recordname(
+                    node,decoration), experiment.ipv6(node))
+
+
+def export_mlab_zone_records(output, sites, experiments):
+    export_router_and_switch_records(output, sites)
+    export_pcu_records(output, sites)
+    export_server_records(output, sites)
+    export_experiment_records(output, sites, experiments)
+
+
+def get_revision(prefix, revision_path):
+    """Returns a two digit revision number as a string.
+
+    The same revision_path should be provided for each call. If the prefix
+    matches the previous prefix, then revision number is incremented by one and
+    returned, otherwise, the revision is zero ("00"). However, the revision
+    number never increases beyond "99".
+
+    Args:
+        prefix: str, current date prefix as YYYYMMDD.
+        revision_path: str, the full path to a temporary file to read previous
+            and store latest revision number.
+
+    Returns:
+        str, a two digit revision number, e.g. "00", "01", etc, up to "99".
+    """
+    n = {'prefix': prefix, 'revision': 0}
+    if os.path.exists(revision_path):
+        with open(revision_path) as f:
+            try:
+                n = json.loads(f.read())
+                if n['prefix'] == prefix:
+                    # Increment previous revision, since prefix is the same.
+                    if n['revision'] < 99:
+                        n['revision'] += 1
+                    else:
+                        logging.error('Revision is too large to increase!')
+                else:
+                    # Reset prefix and revision.
+                    n['prefix'] = prefix
+                    n['revision'] = 0
+            except ValueError:
+                logging.error('Content of %s is corrupted', revision_path)
+
+    revision = '%02d' % n['revision']
+    with open(revision_path, 'w') as f:
+        f.write(json.dumps(n))
+
+    return revision
+
+
+def serial_rfc1912(ts):
+    """Returns an rfc1912 style serial id (YYYYMMDDnn) for a DNS zone file."""
+    # RFC1912 (http://www.ietf.org/rfc/rfc1912.txt) recommends 'nn' as the
+    # revision. However, identifying and incrementing this value is a manual,
+    # error prone step. Instead, we save a temporary daily sequence counter.
+    serial_prefix = time.strftime('%Y%m%d', ts)
+    return serial_prefix + get_revision(serial_prefix, ZONE_SERIAL_COUNTER)
+
+
+def export_mlab_zone_header(output, header, options):
+    """Writes the zone header file to output.
+
+    Data read from header is used as a template and populated with values from
+    options. The end result is written to output.
+
+    Args:
+        output: file, a file object open for writing.
+        header: file, a file object open for reading.
+        options: optparse.Values, all command line options.
+    """
+    headerdata = header.read()
+    headerdata = headerdata % options.__dict__
+    output.write(headerdata)
 
 
 def export_mlab_site_stats(output, sites):
@@ -111,10 +317,14 @@ def main():
     elif options.format == 'sitestats':
         export_mlab_site_stats(sys.stdout, sites)
     elif options.format == 'zone':
-        print 'Sorry, mlabconfig does not yet support generating zone files.'
-        sys.exit(1)
+        with open(options.zoneheader, 'r') as header:
+            if options.serial == 'auto':
+                options.serial = serial_rfc1912(time.gmtime())
+            export_mlab_zone_header(sys.stdout, header, options)
+            sys.stdout.write("\n\n")
+            export_mlab_zone_records(sys.stdout, sites, experiments)
     else:
-        print 'Sorry, unknown format: %s' % options.format
+        logging.error('Sorry, unknown format: %s', options.format)
         sys.exit(1)
 
 
--- plsync/mlabconfig_test.py
+++ plsync/mlabconfig_test.py
@@ -1,10 +1,14 @@
 """Tests for mlabconfig."""
 
-import mlabconfig
-import StringIO
-import unittest
-from planetlab import model
 import json
+import mlabconfig
+import mock
+import optparse
+import os
+from planetlab import model
+import StringIO
+import time
+import unittest
 
 
 class MlabconfigTest(unittest.TestCase):
@@ -57,6 +61,168 @@ class MlabconfigTest(unittest.TestCase):
 
         results = json.loads(output.getvalue())
         self.assertItemsEqual(results, expected_results)
+
+    def test_export_router_and_switch_records(self):
+        output = StringIO.StringIO()
+        expected_results = [
+            mlabconfig.format_a_record('r1.abc01', '192.168.1.1'),
+            mlabconfig.format_a_record('s1.abc01', '192.168.1.2'),
+        ]
+
+        mlabconfig.export_router_and_switch_records(output, self.sites)
+
+        results = output.getvalue().split('\n')
+        self.assertContainsItems(results, expected_results)
+
+    def test_export_pcu_records(self):
+        output = StringIO.StringIO()
+        expected_results = [
+            mlabconfig.format_a_record('mlab1d.abc01', '192.168.1.4'),
+            mlabconfig.format_a_record('mlab2d.abc01', '192.168.1.5'),
+            mlabconfig.format_a_record('mlab3d.abc01', '192.168.1.6'),
+        ]
+
+        mlabconfig.export_pcu_records(output, self.sites)
+
+        results = output.getvalue().split('\n')
+        self.assertContainsItems(results, expected_results)
+
+    def test_export_server_records(self):
+        output = StringIO.StringIO()
+        # This is a subset of expected results.
+        expected_results = [
+            mlabconfig.format_a_record('mlab1.abc01', '192.168.1.9'),
+            mlabconfig.format_a_record('mlab2v4.abc01', '192.168.1.22'),
+            mlabconfig.format_aaaa_record('mlab3.abc01', '2400:1002:4008::35'),
+            mlabconfig.format_aaaa_record('mlab1v6.abc01', '2400:1002:4008::9')
+        ]
+
+        mlabconfig.export_server_records(output, self.sites)
+
+        results = output.getvalue().split('\n')
+        self.assertContainsItems(results, expected_results)
+
+    def test_export_experiment_records(self):
+        output = StringIO.StringIO()
+        experiments = [model.Slice(name='abc_bar', index=1, attrs=self.attrs,
+                                   users=self.users, use_initscript=True,
+                                   ipv6='all')]
+        expected_results = [
+            mlabconfig.format_a_record(
+                'bar.abc.abc01', '192.168.1.11'),
+            mlabconfig.format_a_record(
+                'bar.abc.mlab2.abc01', '192.168.1.24'),
+            mlabconfig.format_a_record(
+                'bar.abcv4.abc01', '192.168.1.11'),
+            mlabconfig.format_a_record(
+                'bar.abc.mlab2v4.abc01', '192.168.1.24'),
+            mlabconfig.format_aaaa_record(
+                'bar.abc.abc01', '2400:1002:4008::11'),
+            mlabconfig.format_aaaa_record(
+                'bar.abc.abc01', '2400:1002:4008::37'),
+            mlabconfig.format_aaaa_record(
+                'bar.abc.mlab3.abc01', '2400:1002:4008::37'),
+            mlabconfig.format_aaaa_record(
+                'bar.abcv6.abc01', '2400:1002:4008::11'),
+            mlabconfig.format_aaaa_record(
+                'bar.abc.mlab1v6.abc01', '2400:1002:4008::11'),
+        ]
+
+        mlabconfig.export_experiment_records(output, self.sites, experiments)
+
+        results = output.getvalue().split('\n')
+        self.assertContainsItems(results, expected_results)
+
+    @mock.patch.object(mlabconfig, 'get_revision')
+    def test_serial_rfc1912(self, mock_get_revision):
+        # Fri Oct 31 00:45:00 2015 UTC.
+        # 45-minutes should result in 03.
+        ts = 1446252300
+        mock_get_revision.return_value = '03'
+
+        serial = mlabconfig.serial_rfc1912(time.gmtime(ts))
+
+        self.assertEqual('2015103103', serial)
+
+    @mock.patch.object(os.path, 'exists')
+    @mock.patch('__builtin__.open')
+    def test_get_revision_when_saved_prefix_is_old_and_revision_is_reset(
+        self, mopen, mock_exists):
+        prefix = '20151031'
+        # All open and disk I/O is mocked out.
+        # Pretend the file exists already.
+        mock_exists.return_value = True
+        # Hold the fake writer so we can check how it was called.
+        mock_writer = mock.mock_open()
+        # Open is called twice, once to read, and then to write.
+        mopen.side_effect = [
+            mock.mock_open(
+                # Saved prefix is older than current prefix.
+                read_data='{"prefix": "20140931", "revision": 1}').return_value,
+            mock_writer.return_value
+        ]
+
+        s = mlabconfig.get_revision(prefix, '/tmp/fakepath')
+
+        self.assertEqual(s, '00')
+        mock_writer.return_value.write.assert_called_once_with(
+            '{"prefix": "20151031", "revision": 0}')
+
+    @mock.patch.object(os.path, 'exists')
+    @mock.patch('__builtin__.open')
+    def test_get_revision_when_file_exists_increments_revision(
+        self, mopen, mock_exists):
+        prefix = '20151031'
+        # All open and disk I/O is mocked out.
+        # Pretend the file exists already.
+        mock_exists.return_value = True
+        # Hold the fake writer so we can check how it was called.
+        mock_writer = mock.mock_open()
+        # Open is called twice, once to read, and then to write.
+        mopen.side_effect = [
+            mock.mock_open(
+                read_data='{"prefix": "20151031", "revision": 1}').return_value,
+            mock_writer.return_value
+        ]
+
+        s = mlabconfig.get_revision(prefix, '/tmp/fakepath')
+
+        self.assertEqual(s, '02')
+        mock_writer.return_value.write.assert_called_once_with(
+            '{"prefix": "20151031", "revision": 2}')
+
+    @mock.patch.object(os.path, 'exists')
+    @mock.patch('__builtin__.open')
+    def test_get_revision_when_file_is_corrupt_default_values_saved(
+        self, mopen, mock_exists):
+        prefix = '20151031'
+        # All open and disk I/O is mocked out.
+        # Pretend the file exists already.
+        mock_exists.return_value = True
+        # Hold the fake writer so we can check how it was called.
+        mock_writer = mock.mock_open()
+        # Open is called twice, once to read, and then to write.
+        mopen.side_effect = [
+            mock.mock_open(
+                read_data='THIS IS NOT JSON').return_value,
+            mock_writer.return_value
+        ]
+
+        s = mlabconfig.get_revision(prefix, '/tmp/fakepath')
+
+        self.assertEqual(s, '00')
+        mock_writer.return_value.write.assert_called_once_with(
+            '{"prefix": "20151031", "revision": 0}')
+
+    def test_export_mlab_zone_header(self):
+        options = optparse.Values()
+        options.value = 'middle'
+        output = StringIO.StringIO()
+        header = StringIO.StringIO('before; %(value)s; after')
+
+        mlabconfig.export_mlab_zone_header(output, header, options)
+
+        self.assertEqual(output.getvalue(), 'before; middle; after')
 
 
 if __name__ == '__main__':
