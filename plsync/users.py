0bed6632a3c5ba86821136a3c156c08269394e4c
Chris Ritzo <critzo@opentechinstitute.org>
2017-05-05 10:25:04 -0400

    removed former operators from users.py

--- plsync/users.py
+++ plsync/users.py
@@ -3,12 +3,9 @@
 user_list = [('Stephen', 'Stuart', 'sstuart@google.com'),
              ('Chris',   'Ritzo', 'critzo@opentechinstitute.org'),
              ('Josh',    'Bailey', 'joshb@google.com'),
-             ('Steph', 'Alarcon', 'salarcon@measurementlab.net'),
              ('Nathan', 'Kinkade', 'kinkade@opentechinstitute.org'),
              ('Matt', 'Mathis', 'mattmathis@google.com'),
              ('Peter', 'Boothe', 'pboothe@google.com'),
-             ('Fernanda', 'Lavalle', 'lavalle@opentechinstitute.org'),
-             ('Michael', 'Lynch', 'mtlynch@google.com'),
              ('Greg', 'Russell', 'gfr@google.com'),
              ('Josh', 'King', 'jking@chambana.net'),
              ('M-Lab', 'Automation', 'support@measurementlab.net')]
