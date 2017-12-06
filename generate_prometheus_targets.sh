7008089df786943d47b929c455a3eb26af125708
Stephen Soltesz <stephen.soltesz@gmail.com>
2017-12-06 17:21:54 -0500

    Separate scraper and global monitoring target generation (#176)
    * Add master & tags deployment support for global prometheus target group
    
    * Support a monitoring group parameter
    
    * Enable monitoring group support

--- generate_prometheus_targets.sh
+++ generate_prometheus_targets.sh
@@ -5,6 +5,9 @@
 SCRIPTDIR=$( dirname "${BASH_SOURCE[0]}" )
 BASEDIR=${PWD}
 
+USAGE="Usage: $0 <GROUP>"
+GROUP=${1:?Please provide monitoring group name: $USAGE}
+
 # Create all output directories.
 for project in mlab-sandbox mlab-staging mlab-oti ; do
   mkdir -p ${BASEDIR}/gen/${project}/prometheus/{legacy-targets,blackbox-targets,snmp-targets}
@@ -27,82 +30,89 @@ for project in mlab-sandbox mlab-staging mlab-oti ; do
     # Construct the per-project SELECT variable name to use below.
     pattern=SELECT_${project/-/_}
 
-    # Rsyncd on port 7999.
-    ./mlabconfig.py --format=prom-targets \
-        --template_target={{hostname}}:7999 \
-        --label service=rsyncd \
-        --label module=rsyncd_online \
-        --rsync \
-        --select "${!pattern}" > ${output}/blackbox-targets/rsyncd.json
+    if [[ ${GROUP} == scraper ]] ; then
+      # Rsyncd on port 7999.
+      ./mlabconfig.py --format=prom-targets \
+          --template_target={{hostname}}:7999 \
+          --label service=rsyncd \
+          --label module=rsyncd_online \
+          --rsync \
+          --select "${!pattern}" > ${output}/blackbox-targets/rsyncd.json
 
-    # SSH on port 806.
-    ./mlabconfig.py --format=prom-targets-nodes \
-        --template_target={{hostname}}:806 \
-        --label service=ssh806 \
-        --label module=ssh_v4_online \
-        --select="${!pattern}" > ${output}/blackbox-targets/ssh806.json
+      # SSH on port 806.
+      ./mlabconfig.py --format=prom-targets-nodes \
+          --template_target={{hostname}}:806 \
+          --label service=ssh806 \
+          --label module=ssh_v4_online \
+          --select="${!pattern}" > ${output}/blackbox-targets/ssh806.json
 
-    # NDT "raw" on port 3001.
-    ./mlabconfig.py --format=prom-targets \
-        --template_target={{hostname}}:3001 \
-        --label service=ndt_raw \
-        --label module=tcp_v4_online \
-        --select="ndt.iupui.(${!pattern})" > \
-            ${output}/blackbox-targets/ndt_raw.json
+      # Sidestream exporter in the npad experiment.
+      ./mlabconfig.py --format=prom-targets \
+          --template_target={{hostname}}:9090 \
+          --label service=sidestream \
+          --select "npad.iupui.(${!pattern})" > \
+              ${output}/legacy-targets/sidestream.json
 
-    # NDT SSL on port 3010.
-    ./mlabconfig.py --format=prom-targets \
-        --template_target={{hostname}}:3010 \
-        --label service=ndt_ssl \
-        --label module=tcp_v4_tls_online \
-        --use_flatnames \
-        --select="ndt.iupui.(${!pattern})" > \
-            ${output}/blackbox-targets/ndt_ssl.json
+    elif [[ ${GROUP} == global ]] ; then
 
-    # inotify_exporter for NDT on port 9393.
-    ./mlabconfig.py --format=prom-targets \
-        --template_target={{hostname}}:9393 \
-        --label service=inotify \
-        --select="ndt.iupui.(${!pattern})" > \
-            ${output}/legacy-targets/ndt_inotify.json
+      # NDT "raw" on port 3001.
+      ./mlabconfig.py --format=prom-targets \
+          --template_target={{hostname}}:3001 \
+          --label service=ndt_raw \
+          --label module=tcp_v4_online \
+          --select="ndt.iupui.(${!pattern})" > \
+              ${output}/blackbox-targets/ndt_raw.json
 
-    # Mobiperf on ports 6001, 6002, 6003.
-    ./mlabconfig.py --format=prom-targets \
-        --template_target={{hostname}}:6001 \
-        --template_target={{hostname}}:6002 \
-        --template_target={{hostname}}:6003 \
-        --label service=mobiperf \
-        --label module=tcp_v4_online \
-        --select="1.michigan.(${!pattern})" > \
-            ${output}/blackbox-targets/mobiperf.json
+      # NDT SSL on port 3010.
+      ./mlabconfig.py --format=prom-targets \
+          --template_target={{hostname}}:3010 \
+          --label service=ndt_ssl \
+          --label module=tcp_v4_tls_online \
+          --use_flatnames \
+          --select="ndt.iupui.(${!pattern})" > \
+              ${output}/blackbox-targets/ndt_ssl.json
 
-    # neubot on port 9773.
-    ./mlabconfig.py --format=prom-targets \
-        --template_target={{hostname}}:9773/sapi/state \
-        --label service=neubot \
-        --label module=neubot_online \
-        --select="neubot.mlab.(${!pattern})" > \
-            ${output}/blackbox-targets/neubot.json
+      # inotify_exporter for NDT on port 9393.
+      ./mlabconfig.py --format=prom-targets \
+          --template_target={{hostname}}:9393 \
+          --label service=inotify \
+          --select="ndt.iupui.(${!pattern})" > \
+              ${output}/legacy-targets/ndt_inotify.json
 
-    # snmp_exporter on port 9116.
-    ./mlabconfig.py --format=prom-targets-sites \
-        --template_target=s1.{{sitename}}.measurement-lab.org \
-        --label service=snmp \
-        --label __exporter_project=${project#mlab-} > \
-            ${output}/snmp-targets/snmpexporter.json
+      # Mobiperf on ports 6001, 6002, 6003.
+      ./mlabconfig.py --format=prom-targets \
+          --template_target={{hostname}}:6001 \
+          --template_target={{hostname}}:6002 \
+          --template_target={{hostname}}:6003 \
+          --label service=mobiperf \
+          --label module=tcp_v4_online \
+          --select="1.michigan.(${!pattern})" > \
+              ${output}/blackbox-targets/mobiperf.json
 
-    # Sidestream exporter in the npad experiment.
-    ./mlabconfig.py --format=prom-targets \
-        --template_target={{hostname}}:9090 \
-        --label service=sidestream \
-        --select "npad.iupui.(${!pattern})" > \
-            ${output}/legacy-targets/sidestream.json
+      # neubot on port 9773.
+      ./mlabconfig.py --format=prom-targets \
+          --template_target={{hostname}}:9773/sapi/state \
+          --label service=neubot \
+          --label module=neubot_online \
+          --select="neubot.mlab.(${!pattern})" > \
+              ${output}/blackbox-targets/neubot.json
 
-    # node_exporter on port 9100.
-    ./mlabconfig.py --format=prom-targets-nodes \
-        --template_target={{hostname}}:9100 \
-        --label service=nodeexporter \
-        --label module=lame_duck > \
-            ${output}/legacy-targets/lameduck.json
+      # snmp_exporter on port 9116.
+      ./mlabconfig.py --format=prom-targets-sites \
+          --template_target=s1.{{sitename}}.measurement-lab.org \
+          --label service=snmp \
+          --label __exporter_project=${project#mlab-} > \
+              ${output}/snmp-targets/snmpexporter.json
+
+      # node_exporter on port 9100.
+      ./mlabconfig.py --format=prom-targets-nodes \
+          --template_target={{hostname}}:9100 \
+          --label service=nodeexporter \
+          --label module=lame_duck > \
+              ${output}/legacy-targets/lameduck.json
+
+    else
+      echo "Unknown group name: ${GROUP} for ${project}"
+    fi
   popd
 done
