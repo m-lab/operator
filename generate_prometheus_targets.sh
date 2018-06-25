f00401d58030991e9b8396f0fb5ae965db18ca98
nkinkade <nath@nkinka.de>
2018-06-25 13:29:09 -0600

    Adds a ping probe for platform switches (#236)
    * Adds target generation for a blackbox_exporter ICMP probe of the platform switches.
    
    * Removes the obsolete, unused label __exporter_project from the snmpexporter.json targets. This label is a vestige from the time before we templated Prometheus configs.

--- generate_prometheus_targets.sh
+++ generate_prometheus_targets.sh
@@ -175,8 +175,7 @@ for project in mlab-sandbox mlab-staging mlab-oti ; do
       # snmp_exporter on port 9116.
       ./mlabconfig.py --format=prom-targets-sites \
           --template_target=s1.{{sitename}}.measurement-lab.org \
-          --label service=snmp \
-          --label __exporter_project=${project} > \
+          --label service=snmp > \
               ${output}/snmp-targets/snmpexporter.json
 
       # inotify_exporter for NDT on port 9393.
@@ -192,6 +191,12 @@ for project in mlab-sandbox mlab-staging mlab-oti ; do
           --label service=nodeexporter \
           --select "${!pattern}" > \
               ${output}/legacy-targets/nodeexporter.json
+
+      # ICMP probe for platform switches
+      ./mlabconfig.py --format=prom-targets-sites \
+          --template_target=s1.{{sitename}}.measurement-lab.org \
+          --label module=icmp > \
+              ${output}/blackbox-targets/switches_ping.json
 
     else
       echo "Unknown group name: ${GROUP} for ${project}"
