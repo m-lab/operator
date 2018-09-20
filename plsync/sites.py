f865b1119666b9b9bafb1b4dc523d937c7ad1282
nkinkade <nath@nkinka.de>
2018-09-20 09:29:19 -0600

    Updates site details for TRN01, which will soon be 4 nodes with new hardware. (#255)
    * Updates site details for TRN01, which will soon be 4 nodes with new hardware.
    
    * Turns on roundrobin for TRN01, which will have new hardware shortly.

--- plsync/sites.py
+++ plsync/sites.py
@@ -178,7 +178,7 @@ site_list = [
     makesite('tgd01','213.149.127.0',   '2a00:fe80:0:104::',   'Podgorica', 'ME', 42.107764, 18.761649, user_list, count=4, arch='x86_64-r630', nodegroup='MeasurementLabCentos', roundrobin=True),
     makesite('tnr01','41.188.12.64',   None,                   'Antananarivo', 'MG', -18.7969, 47.4788, user_list, exclude=[1,2,3,4], count=4, arch='x86_64-r630', nodegroup='MeasurementLabCentos', roundrobin=False),
     makesite('tpe01','163.22.28.0',    '2001:e10:6840:28::',   'Taipei', 'TW', 25.077800, 121.224000, user_list, nodegroup='MeasurementLabCentos', roundrobin=False),
-    makesite('trn01','194.116.85.192', '2001:7f8:23:307::',    'Turin', 'IT', 45.200800, 7.649720, user_list, nodegroup='MeasurementLabCentos', roundrobin=False),
+    makesite('trn01','194.116.85.192', '2001:7f8:23:307::',    'Turin', 'IT', 45.200800, 7.649720, user_list, count=4, arch='x86_64-r630', nodegroup='MeasurementLabCentos', roundrobin=True),
     # old ipv6 2c0f:fab0:ffff:1000:: @ tun01
     makesite('tun01','41.231.21.0',    '2001:4350:3000:1::',   'Tunis', 'TN', 36.851600, 10.229100, user_list, count=4, arch='x86_64-r630',  nodegroup='MeasurementLabCentos', roundrobin=True),
     makesite('vie01','213.208.152.0',  '2a01:190:1700:38::',   'Vienna', 'AT', 48.269000, 16.410700, user_list, count=4, arch='x86_64-r630', nodegroup='MeasurementLabCentos', roundrobin=True),
